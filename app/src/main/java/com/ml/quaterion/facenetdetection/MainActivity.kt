af8650a608be13baa49cabef2091aca904a7ade5
RageF1ghter <czybaba@gmail.com>
2023-04-25 22:07:12 -0700

    final project

--- app/src/main/java/com/ml/quaterion/facenetdetection/MainActivity.kt
+++ app/src/main/java/com/ml/quaterion/facenetdetection/MainActivity.kt
@@ -145,7 +145,7 @@ class MainActivity : AppCompatActivity() {
                             val subListRef = storage.reference.child("faces/${subfolder.name}")
                             val images = subListRef.listAll().await()
                             images.items.forEach{ image->
-                                val ONE_MEGABYTE: Long = 1024 * 1024
+                                val ONE_MEGABYTE: Long = 1024 * 1024 * 10
                                 val imageByte = image.getBytes(ONE_MEGABYTE).await()
                                 val imageBitmap = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.size)
                                 imagesData.add(Pair(subfolder.name, imageBitmap))
@@ -180,7 +180,7 @@ class MainActivity : AppCompatActivity() {
                             val images = subListRef.listAll().await()
                             images.items.forEach{ image->
 
-                                val MEGABYTE: Long = 1024 * 1024 * 10   //define the max size of image per fetching
+                                val MEGABYTE: Long = 1024 * 1024 * 10
                                 val imageByte = image.getBytes(MEGABYTE).await()
                                 val imageBitmap = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.size)
                                 imagesData.add(Pair(subfolder.name, imageBitmap))
